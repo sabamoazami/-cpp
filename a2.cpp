6cace352b90a92d805c5d83fcd7b14296323a7c9
sabamoazami <46655880+sabamoazami@users.noreply.github.com>
2019-01-16 04:20:44 -0800

    Add files via upload

---
+++ a2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+	int num;
+	cin>> num;
+	switch (num)
+	{
+	
+	case 0:
+	cout<<"sunday";
+		break;
+	case 1:
+	cout<<"monday";
+	    break;
+	case 2:
+	cout<<"tuesday";
+	    break;
+	case 3:
+	cout<<"wednesday";
+	    break;
+	case 4:
+	cout<<"thursday";
+	    break;
+	case 5:
+	cout<<"friday";
+		break;
+	case 6:
+	cout<<"saturday";
+		break;    
+	}
+	getch();		
+}
