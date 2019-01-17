bb552880b7ea795404a0af25d4d538011daba49b
sabamoazami <46655880+sabamoazami@users.noreply.github.com>
2019-01-17 02:27:05 -0800

    Add files via upload

---
+++ say hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+ void sayHello( string name){
+	cout<< "Hello "<<name<<" :)";
+}
+   int main(){
+   	 string name;
+   	 cin>> name;
+   	 sayHello(name);
+   }
